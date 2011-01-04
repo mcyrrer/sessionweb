aa8e8f9130a9ed144e152847956d6dcf9946cd0e
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2011-01-04 15:00:19 +0000

    added form validation in javascript to session.php

---
+++ js/sessionwebjs.js
@@ -0,0 +1,70 @@
+function validate_required(field, alerttxt) {
+	with (field) {
+		if (value == null || value == "") {
+			alert(alerttxt);
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
+
+function validate_metrics(setuppercent, testpercent, bugpercent,
+		oppertunitypercent, executed) {
+	
+	if (executed.checked == 1) {
+		var setup;
+		var test;
+		var bug;
+		var oppertunity;
+
+		with (setuppercent) {
+			setup = value;
+		}
+
+		with (testpercent) {
+			test = value;
+		}
+
+		with (setuppercent) {
+			setup = value;
+		}
+
+		with (bugpercent) {
+			bug = value;
+		}
+
+		with (oppertunitypercent) {
+			oppertunity = value;
+		}
+
+		var percent = parseInt(setup) + parseInt(test) + parseInt(bug)
+				+ parseInt(oppertunity);
+		if (parseInt(percent) != 100) {
+			alert("Percentage for session is " + parseInt(percent)
+					+ "%. It has to be 100%.");
+			return false;
+		} else {
+			return true;
+		}
+	} else {
+		return true;
+	}
+
+}
+
+function validate_form(thisform) {
+	with (thisform) {
+		if (validate_required(title, "Title must be filled out!") == false) {
+			title.focus();
+			return false;
+		}
+
+		if (validate_metrics(setuppercent, testpercent, bugpercent,
+				oppertunitypercent, executed) == false) {
+			setuppercent.focus();
+			return false;
+		}
+
+	}
+}
