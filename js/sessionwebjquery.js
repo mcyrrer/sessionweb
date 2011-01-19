7deb87eda6759ca31d058d30b4bedda027266036
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2011-01-19 15:40:42 +0000

    Updated js for add/remove bug i session.php

--- js/sessionwebjquery.js
+++ js/sessionwebjquery.js
@@ -1,56 +1,67 @@
 $buglist = "";
-$(document)
-		.ready(
-				function() {
+$(document).ready(function(){
 
-					$("#option_list").hide();
+    $("#option_list").hide();
+    var myBugs = new Array();
+    
+    // Metrics calculation
+    $("[class=metricoption]").change(function(){
+        var totalPercentage = parseInt($("[name=oppertunitypercent]").val()) +
+        parseInt($("[name=bugpercent]").val()) +
+        parseInt($("[name=testpercent]").val()) +
+        parseInt($("[name=setuppercent]").val());
+        if (totalPercentage != 100) {
+            $("#metricscalculation").html("<div id=\"metricscalculation_red\">Total percentage = " +
+            totalPercentage +
+            "%. Please adjust it to 100%.</div>");
+        }
+        else {
+            $("#metricscalculation").html("Percentage = " +
+            totalPercentage +
+            "%");
+        }
+    });
+    
+    // Show search option in list.php
+    $("#showoption").click(function(){
+        $("#option_list").fadeIn("slow");
+    });
+    
+    // Add bug to session.
+    $("#add_bug").click(function(){
+        $bug = $("#bug").val()+'';
+        
+        myBugs.push($bug);
+        
+        
+        $("<div id=\"bug_" + $bug + "\" class=\"test\"><p>" + $bug + "</p></div>").appendTo('#buglist_visible');
+        alert(myBugs);
+        for (var i = 0; i < myBugs.length; i++) {
+            var value = myBugs[i];
+            
 
-					// Metrics calculation
-					$("[class=metricoption]")
-							.change(
-									function() {
-										var totalPercentage = parseInt($(
-												"[name=oppertunitypercent]")
-												.val())
-												+ parseInt($(
-														"[name=bugpercent]")
-														.val())
-												+ parseInt($(
-														"[name=testpercent]")
-														.val())
-												+ parseInt($(
-														"[name=setuppercent]")
-														.val());
-										if (totalPercentage != 100) {
-											$("#metricscalculation")
-													.html(
-															"<div id=\"metricscalculation_red\">Total percentage = "
-																	+ totalPercentage
-																	+ "%. Please adjust it to 100%.</div>");
-										} else {
-											$("#metricscalculation").html(
-													"Percentage = "
-															+ totalPercentage
-															+ "%");
-										}
-									});
-	
-					// Show search option in list.php
-					$("#showoption").click(function() {
-							$("#option_list").fadeIn("slow");
-					});
-					
-					
-					// Add bug to session.
-					$("#add_bug").click(function(){
-						$thisid = this.id; //Good to have when we want to remove bugs from list.....
-						$bug = $("#bug").val();
-						$buglist = $buglist + $bug + "|";
-		                $('#buglist_visible').append($bug+"<br>");
-						$('#buglist_hidden').text($buglist);
-		            });
+            $("#bug_" + value + "").click(function(){
+                $("#bug_" + value + "").remove();
+                myBugs.remove(value);
+            });
+        }
+        
+        
+        
+    });
+    
+    //        $thisid = this.id; //Good to have when we want to remove bugs from list.....
+    //        $bug = $("#bug").val();
+    //        $buglist = $buglist + $bug + "|";
+    //        
+    //        var newTextBoxDiv = $(document.createElement('div')).attr("id", 'TextBoxDiv' + $bug);
+    //        newTextBoxDiv.after().html("test!!!");
+    //        
+    //        
+    //        //        newTextBoxDiv.appendTo("#buglist_visible");
+    //        $('#buglist_visible').append(newTextBoxDiv);
+    //        
+    //        //        $('#buglist_visible').append($bug + "<br>");
+    //        $('#buglist_hidden').text($buglist);
 
-
-				}
-
-		);
+});
