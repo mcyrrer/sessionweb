2fee69bc077d0aa01c2a423b2fc7307276016a2d
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2011-01-20 07:13:58 +0000

    added requirements to session.php

--- js/sessionwebjquery.js
+++ js/sessionwebjquery.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 
     $("#option_list").hide();
     var myBugs = new Array();
+	var myRequirements = new Array();
     
     // Metrics calculation
     $("[class=metricoption]").change(function(){
@@ -30,28 +31,50 @@ $(document).ready(function(){
     // Add bug to session and manage if it is deleted
     $("#add_bug").click(function(){
         var bugValue = $("#bug").val() + '';
-        if (jQuery.inArray(bugValue, myBugs) == -1 && bugValue != "") {
+        if (jQuery.inArray(bugValue, myBugs) == -1 &&
+        bugValue != "") {
             myBugs.push(bugValue);
             $("#bug").attr('value', '');
             
-            var newBugHtml = "<div id=\"bugdiv_" + bugValue + "\">"
-            + "  <a href=\"http://" + bugValue + "\" class=\"bugurl\" target=\”_blank\”>" + bugValue + "</a>"
-            + "  <div id=\"bug_" + bugValue + "\"> [remove]</div>"
-            + "</div>";
-
+            var newBugHtml = "<div id=\"bugdiv_" +
+            bugValue +
+            "\">" +
+            "<table width=\"200\" border=\"0\">" +
+            "    <tr>" +
+            "        <td><a href=\"" +
+            url_to_dms +
+            "" +
+            bugValue +
+            "\" class=\"bugurl\" target=\”_blank\”>" +
+            bugValue +
+            "</a>" +
+            "        </td>" +
+            "        <td><div id=\"bug_" +
+            bugValue +
+            "\"> <img src=\"pictures/removeicon.png\" alt=\"[remove]\" /></div>" +
+            "        </td>" +
+            "    </tr>" +
+            "</table>" +
+            "</div>";
+            
             $(newBugHtml).appendTo('#buglist_visible');
             
             $('#buglist_hidden').text(myBugs.toString());
             $("#bug_" + bugValue + "").click(function(){
                 var thisIe = this.id;
-                var bugUrlId = "bugdiv_" + bugValue;
+                var bugUrlId = "bugdiv_" +
+                bugValue;
                 if (this.id != bugUrlId) {
-                    var answer = confirm("Remove bug " + bugValue + "?")
+                    var answer = confirm("Remove bug " +
+                    bugValue +
+                    "?")
                     if (answer) {
-                        $("#bugdiv_" + bugValue + "").remove();
+                        $("#bugdiv_" +
+                        bugValue +
+                        "").remove();
                         bugPos = jQuery.inArray(bugValue, myBugs);
                         if (bugPos != -1) {
-                            var removedelements = myBugs.splice(bugPos, 1);//remove();
+                            var removedelements = myBugs.splice(bugPos, 1);// remove();
                             $('#buglist_hidden').text(myBugs.toString());
                         }
                     }
@@ -59,14 +82,78 @@ $(document).ready(function(){
             });
         }
         else {
-            if (bugValue == "") {
-                //				alert("Bug with id " + bugValue + " is already connected to session.");
-            }
-            else {
-                alert("Bug with id " + bugValue + " is already connected to session.");
-            }
+			 if (bugValue == "") {
+			 }
+			 else {
+			 	alert("Bug with id " +
+			 	bugValue +
+			 	" is already connected to session.");
+			 }
         }
     });
-    
+	
+    // Add requirement to session and manage if it is deleted
+    $("#add_requirement").click(function(){
+        var requirementValue = $("#requirement").val() + '';
+        if (jQuery.inArray(requirementValue, myRequirements) == -1 &&
+        requirementValue != "") {
+            myRequirements.push(requirementValue);
+            $("#requirement").attr('value', '');
+            
+            var newrequirementHtml = "<div id=\"requirementdiv_" +
+            requirementValue +
+            "\">" +
+            "<table width=\"200\" border=\"0\">" +
+            "    <tr>" +
+            "        <td><a href=\"" +
+            url_to_rms +
+            "" +
+            requirementValue +
+            "\" class=\"requirementurl\" target=\”_blank\”>" +
+            requirementValue +
+            "</a>" +
+            "        </td>" +
+            "        <td><div id=\"requirement_" +
+            requirementValue +
+            "\"> <img src=\"pictures/removeicon.png\" alt=\"[remove]\" /></div>" +
+            "        </td>" +
+            "    </tr>" +
+            "</table>" +
+            "</div>";
+            
+            $(newrequirementHtml).appendTo('#requirementlist_visible');
+            
+            $('#requirementlist_hidden').text(myRequirements.toString());
+            $("#requirement_" + requirementValue + "").click(function(){
+                var thisIe = this.id;
+                var requirementUrlId = "requirementdiv_" +
+                requirementValue;
+                if (this.id != requirementUrlId) {
+                    var answer = confirm("Remove requirement " +
+                    requirementValue +
+                    "?")
+                    if (answer) {
+                        $("#requirementdiv_" +
+                        requirementValue +
+                        "").remove();
+                        requirementPos = jQuery.inArray(requirementValue, myRequirements);
+                        if (requirementPos != -1) {
+                            var removedelements = myRequirements.splice(requirementPos, 1);// remove();
+                            $('#requirementlist_hidden').text(myRequirements.toString());
+                        }
+                    }
+                }
+            });
+        }
+        else {
+             if (requirementValue == "") {
+             }
+             else {
+                alert("requirement with id " +
+                requirementValue +
+                " is already connected to session.");
+             }
+        }
+    });
     
 });
