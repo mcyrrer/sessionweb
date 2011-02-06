fdfa143dec786522ac66d665711208afa8d289fd
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2011-02-06 19:14:51 +0000

    public key tc added

---
+++ trunk/Sessionweb-AutoTest/src/test/java/com/sessionweb/autotest/tests/publicView.java
@@ -0,0 +1,43 @@
+package com.sessionweb.autotest.tests;
+
+import com.sessionweb.autotest.CommonSteps;
+import com.sessionweb.autotest.SessionWebTest;
+import com.thoughtworks.selenium.*;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class publicView extends SessionWebTest {
+	CommonSteps cs = new CommonSteps();
+
+	@Test
+	public void viewSessionWithValidKey() throws Exception {
+        cs.cleanDb();
+		cs.logIn(selenium);
+
+		selenium.click("url_newsession");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("input_title");
+		selenium.type("input_title", "test session for public key");
+		selenium.type("requirement", "req");
+		selenium.click("add_requirement");
+		selenium.type("bug", "defect");
+		selenium.select("setuppercent", "label=80");
+		selenium.select("testpercent", "label=5");
+		selenium.select("bugpercent", "label=5");
+		selenium.select("oppertunitypercent", "label=10");
+		selenium.select("duration", "label=90");
+		selenium.click("executed");
+		selenium.click("input_submit");
+		selenium.waitForPageToLoad("30000");
+        //String url = selenium.getText("@viewsessionmenu");
+		selenium.click("view_session");
+		selenium.waitForPageToLoad("30000");
+		assertTrue(selenium.isElementPresent("publiclink"));
+		selenium.click("publiclink");
+		selenium.waitForPageToLoad("30000");
+		assertTrue(selenium.isTextPresent("test session for public key"));
+		assertTrue(selenium.isTextPresent("80"));
+
+		//cs.logOut(selenium);
+	}
+}
