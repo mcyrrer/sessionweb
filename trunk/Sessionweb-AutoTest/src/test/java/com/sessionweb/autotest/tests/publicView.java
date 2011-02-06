a7f8603a5a9481e629c2a7f0139a719613861ca7
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2011-02-06 21:16:44 +0000

    Added test cases for public view

--- trunk/Sessionweb-AutoTest/src/test/java/com/sessionweb/autotest/tests/publicView.java
+++ trunk/Sessionweb-AutoTest/src/test/java/com/sessionweb/autotest/tests/publicView.java
@@ -29,7 +29,6 @@ public class publicView extends SessionWebTest {
 		selenium.click("executed");
 		selenium.click("input_submit");
 		selenium.waitForPageToLoad("30000");
-        //String url = selenium.getText("@viewsessionmenu");
 		selenium.click("view_session");
 		selenium.waitForPageToLoad("30000");
 		assertTrue(selenium.isElementPresent("publiclink"));
@@ -38,6 +37,38 @@ public class publicView extends SessionWebTest {
 		assertTrue(selenium.isTextPresent("test session for public key"));
 		assertTrue(selenium.isTextPresent("80"));
 
-		//cs.logOut(selenium);
+	}
+
+    @Test
+	public void viewSessionWithInValidKey() throws Exception {
+        cs.cleanDb();
+		cs.logIn(selenium);
+
+		selenium.click("url_newsession");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("input_title");
+		selenium.type("input_title", "test session for public key");
+		selenium.type("requirement", "req");
+		selenium.click("add_requirement");
+		selenium.type("bug", "defect");
+		selenium.select("setuppercent", "label=80");
+		selenium.select("testpercent", "label=5");
+		selenium.select("bugpercent", "label=5");
+		selenium.select("oppertunitypercent", "label=10");
+		selenium.select("duration", "label=90");
+		selenium.click("executed");
+		selenium.click("input_submit");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("view_session");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("publiclink");
+		selenium.waitForPageToLoad("30000");
+
+
+        String url = selenium.getLocation();
+        url = url.substring(0,url.length()-1);
+        selenium.open(url);
+        selenium.waitForPageToLoad("30000");
+        assertTrue(selenium.isTextPresent("not valid"));
 	}
 }
