458c323d85e28340d743cf52b8398c779771b497
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2011-12-02 10:37:44 +0000

    buf fixes in 1.4 sql script

--- tags/sessionweb-1.4/install/SessionwebDbLayoutDelta_1.3-_1.4.sql
+++ tags/sessionweb-1.4/install/SessionwebDbLayoutDelta_1.3-_1.4.sql
@@ -2,11 +2,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-ALTER TABLE `sessionwebos`.`mission_attachments` COLLATE = utf8_general_ci , DROP COLUMN `mission_versionid` , CHANGE COLUMN `id` `mission_versionid` INT(11) NOT NULL  
-, DROP INDEX `fk_attach_mission1` 
-, ADD INDEX `fk_attach_mission1` (`mission_versionid` ASC) 
-, DROP INDEX `id_UNIQUE` ;
-
 CREATE  TABLE IF NOT EXISTS `sessionwebos`.`user_sessionsnotification` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `versionid` INT(11) NOT NULL ,
@@ -29,51 +24,7 @@ CREATE  TABLE IF NOT EXISTS `sessionwebos`.`user_sessionsnotification` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = latin1_swedish_ci;
-
-
--- -----------------------------------------------------
--- Placeholder table for view `sessionwebos`.`sessioninfo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sessionwebos`.`sessioninfo` (`sessionid` INT, `versionid` INT, `title` INT, `username` INT, `executed` INT, `debriefed` INT, `closed` INT, `publickey` INT, `updated` INT, `teamname` INT, `sprintname` INT, `executed_timestamp` INT, `debriefed_timestamp` INT, `setup_percent` INT, `test_percent` INT, `bug_percent` INT, `opportunity_percent` INT, `duration_time` INT);
-
-
-USE `sessionwebos`;
-
--- -----------------------------------------------------
--- View `sessionwebos`.`sessioninfo`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `sessionwebos`.`sessioninfo`;
-USE `sessionwebos`;
-CREATE  OR REPLACE VIEW `sessionwebos`.`sessioninfo` AS SELECT 
-        m.sessionid,
-        m.versionid,
-        m.title,
-        m.username,
-        ms.executed,
-        ms.debriefed,
-        ms.closed,
-        m.publickey,
-        m.updated,
-        m.teamname,
-        m.sprintname,
-        ms.executed_timestamp,
-        ms.debriefed_timestamp,
-        sm.setup_percent,
-        sm.test_percent,
-        sm.bug_percent,
-        sm.opportunity_percent,
-        sm.duration_time
-    from
-        mission m,
-        mission_status ms,
-        mission_sessionmetrics sm
-    WHERE
-        m.versionid = ms.versionid
-        AND
-        m.versionid = sm.versionid
-;
+DEFAULT CHARACTER SET = utf8;
 
 UPDATE  `sessionwebos`.`version` SET  `versioninstalled` =  '1.4' WHERE  `version`.`id` =1;
 
