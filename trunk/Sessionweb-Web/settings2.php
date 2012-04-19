cde1fa0c95eb69ba54e4be321271a24a25479c3f
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2012-04-19 20:08:30 +0000

    made improvments to buttons in settings2.php

--- trunk/Sessionweb-Web/settings2.php
+++ trunk/Sessionweb-Web/settings2.php
@@ -51,7 +51,7 @@ $usersettings = getUserSettings();
                             </td>
                         </tr>
                     </table>
-                    <span id='change_personal_password_exe'>CHANGE PASSWORD</span>
+                    <span class='settings_submit' id='change_personal_password_exe'>CHANGE PASSWORD</span>
 
                 </div>
 
@@ -80,7 +80,7 @@ $usersettings = getUserSettings();
                     <?php echoAreaSelectSingel($usersettings['default_area'],false,"personal_select_area");?>
 
                     <div>
-                    <span id='change_personal_settings_exe'>CHANGE SETTINGS</span>
+                    <span class='settings_submit' id='change_personal_settings_exe'>CHANGE SETTINGS</span>
                     </div>
 <!--                    <a href="settings.php">Old settings page</a>-->
                 </div>
@@ -94,13 +94,13 @@ $usersettings = getUserSettings();
                 <div id='add_remove_team'>
                     <div class='divider_settings'></div>
                     <p>Add Team</p>
-                    <input type="text" size="50" value="" id="teamname"> <span id='add_team'>ADD</span>
+                    <input type="text" size="50" value="" id="teamname"> <span class='settings_submit' id='add_team'>ADD</span>
 
                     <div class='divider_settings_noline'></div>
                     <p>Remove Team</p>
 
                     <div>
-                        <select id='remove_team_select'></select> <span id='remove_team'>REMOVE</span>
+                        <select id='remove_team_select'></select> <span class='settings_submit' id='remove_team'>REMOVE</span>
                     </div>
                     <div class='divider_settings'></div>
 
@@ -113,13 +113,13 @@ $usersettings = getUserSettings();
                 <div id='add_remove_sprint'>
                     <div class='divider_settings'></div>
                     <p>Add sprint</p>
-                    <input type="text" size="50" value="" id="sprintname"> <span id='add_sprint'>ADD</span>
+                    <input type="text" size="50" value="" id="sprintname"> <span class='settings_submit' id='add_sprint'>ADD</span>
 
                     <div class='divider_settings_noline'></div>
                     <p>Remove sprint</p>
 
                     <div>
-                        <select id='remove_sprint_select'></select> <span id='remove_sprint'>REMOVE</span>
+                        <select id='remove_sprint_select'></select> <span class='settings_submit' id='remove_sprint'>REMOVE</span>
                     </div>
                     <div class='divider_settings'></div>
                 </div>
@@ -131,13 +131,13 @@ $usersettings = getUserSettings();
                 <div id='add_remove_area'>
                     <div class='divider_settings'></div>
                     <p>Add Area</p>
-                    <input type="text" size="50" value="" id="areaname"> <span id='add_area'>ADD</span>
+                    <input type="text" size="50" value="" id="areaname"> <span class='settings_submit' id='add_area'>ADD</span>
 
                     <div class='divider_settings_noline'></div>
                     <p>Remove Area</p>
 
                     <div>
-                        <select id='remove_area_select'></select> <span id='remove_area'>REMOVE</span>
+                        <select id='remove_area_select'></select> <span class='settings_submit' id='remove_area'>REMOVE</span>
                     </div>
                     <div class='divider_settings'></div>
                 </div>
@@ -155,7 +155,7 @@ $usersettings = getUserSettings();
                     <input type="text" size="50" value="" id="teUrl"><br>
                     Username:<br><input type="text" size="50" value="" id="teUser"><br>
                     Password:<br><input type="text" size="50" value="" id="tePassword"><br>
-                    <span id='add_testenvironment'>ADD</span>
+                    <span class='settings_submit' id='add_testenvironment'>ADD</span>
 
                     <div class='divider_settings_noline'></div>
                     <p>Remove testenvironment</p>
