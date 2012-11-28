51a00980e0f2aded4ce1b5bacda6e15f773b3000
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2012-11-28 07:29:29 +0000

    Release 24

---
+++ tags/sessionweb-24/include/loggingsetup.php
@@ -0,0 +1,50 @@
+<?php
+$logpath = '';
+if (file_exists('include/log4php/Logger.php')) {
+    require_once ('include/log4php/Logger.php');
+    $logpath = 'log/sessionweb.log';
+}
+elseif (file_exists('../include/log4php/Logger.php'))
+{
+    require_once ('../include/log4php/Logger.php');
+    $logpath = '../log/sessionweb.log';
+}
+elseif (file_exists('../../include/log4php/Logger.php'))
+{
+    require_once ('../../include/log4php/Logger.php');
+    $logpath = '../../log/sessionweb.log';
+}
+elseif (file_exists('../../../include/log4php/Logger.php'))
+{
+    require_once ('../../../include/log4php/Logger.php');
+    $logpath = '../../../log/sessionweb.log';
+}
+elseif (file_exists('../../../../include/log4php/Logger.php'))
+{
+    require_once ('../../../../include/log4php/Logger.php');
+    $logpath = '../../../../log/sessionweb.log';
+}
+elseif (file_exists('../../../../../include/log4php/Logger.php'))
+{
+    require_once ('../../../../../include/log4php/Logger.php');
+    $logpath = '../../../../../log/sessionweb.log';
+}
+else
+{
+    echo "Log4Php not found. pleas correct this in the include/loggingsetup.php.";
+    exit();
+}
+
+$logger = Logger::getRootLogger();
+$logger->setLevel(LoggerLevel::INFO);
+
+$appender = new LoggerAppenderFile("MyAppender");
+
+$appender->setFile("$logpath", true);
+$appenderlayout = new LoggerLayoutTTCC();
+$appender->setLayout($appenderlayout);
+$appender->activateOptions();
+
+$logger->removeAllAppenders();
+$logger->addAppender($appender);
+?>
