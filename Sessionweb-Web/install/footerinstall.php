40f3d2dd5235a34c1700c4fd9f9d828c68fd310b
mcyrrer@gmail.com <mcyrrer@gmail.com@38c3524e-8056-9ced-b3ba-7282e16c5493>
2012-12-03 20:08:53 +0000

    installation fixes

--- Sessionweb-Web/install/footerinstall.php
+++ Sessionweb-Web/install/footerinstall.php
@@ -1,14 +1,29 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: matgus
- * Date: 2012-12-02
- * Time: 21:39
- * To change this template use File | Settings | File Templates.
- */
-echo '
-<div id="boldline"></div>
+echo "<br><div id='boldline'></div>";
 
-<div id="footer">Sessionweb ver 24  | <a href="about.php"id="url_about">About</a> | <a href="http://www.sessionweb.org" id="url_sessionweb_prj_page">Project Home Page</a> | <a href="http://code.google.com/p/sessionweb/issues/list" id="url_submintbug">Submit a bug report</a><br>
-</div></div>  </body>
-</html>';
+if (file_exists('../config/db.php.inc')) {
+    require_once ('../config/db.php.inc');
+
+$con=getMySqlConnection();
+mysql_select_db(DB_NAME_SESSIONWEB)or die("Database not available. Please install <a href='install/'>sessionweb</a>");
+$result = mysql_query("SELECT * FROM `version`;");
+
+$row = mysql_fetch_array($result);
+$versionInstalled = $row['versioninstalled'];
+mysql_close($con);
+}
+else
+{
+    $versionInstalled = ": not yet installed :)";
+}
+echo "<div id='footer'>";
+echo "Sessionweb ver $versionInstalled  | ";
+echo "<a href=\"about.php\"id=\"url_about\">About</a> | ";
+echo "<a href=\"http://www.sessionweb.org\" id=\"url_sessionweb_prj_page\">Project Home Page</a> | ";
+echo "<a href=\"http://code.google.com/p/sessionweb/issues/list\" id=\"url_submintbug\">Submit a bug report</a><br>\n";
+echo "</div>";
+echo "</div>";
+echo "  </body>\n";
+echo "</html>\n";
+
+?>
--- Sessionweb-Web/install/upgrade.php
+++ Sessionweb-Web/install/upgrade.php
@@ -5,38 +5,64 @@ session_start();
 //require_once('../include/validatesession.inc');
 require_once('../classes/logging.php');
 require_once('../include/db.php');
+
 include_once ('MySqlExecuter.php');
 include_once ('../include/commonFunctions.php.inc');
-include_once ('../config/db.php.inc');
+if (file_exists('../config/db.php.inc')) {
+    include_once ('../config/db.php.inc');
+}
 $logger = new logging();
+
+$versions = array();
+$versions['1.0'] = "SessionwebDbLayoutDelta_1.0-_1.2.sql";
+$versions['1.2'] = "SessionwebDbLayoutDelta_1.2-_1.3.sql";
+$versions['1.3'] = "SessionwebDbLayoutDelta_1.3-_1.4.sql";
+$versions['1.4'] = "SessionwebDbLayoutDelta_1.4-_1.5.sql";
+$versions['1.5'] = "SessionwebDbLayoutDelta_1.5-_1.6.sql";
+$versions['1.6'] = "SessionwebDbLayoutDelta_1.6-_1.7.sql";
+$versions['1.7'] = "SessionwebDbLayoutDelta_1.7-_18.sql";
+$versions['18'] = "SessionwebDbLayoutDelta_18-_19.sql";
+$versions['19'] = "SessionwebDbLayoutDelta_19-_20.sql";
+$versions['20'] = "SessionwebDbLayoutDelta_20-_21.sql";
+$versions['21'] = "SessionwebDbLayoutDelta_21-_22.sql";
+$versions['22'] = "SessionwebDbLayoutDelta_22-_23.sql";
+$versions['23'] = "SessionwebDbLayoutDelta_23-_24.sql";
+
+
 echo '<div id="container">
     <div><H1>Upgrade of Sessionweb</H1></div>';
-if (isset($_SESSION['useradmin'])) {
-    if ($_SESSION['useradmin'] != 1) {
-        $logger->info('Access of upgrade page not granted! User does not have admin priviliges.', __FILE__, __LINE__);
-        echo "<br><br><br>Admin privilege needed to be able to update sessionweb. Please login using a user that have admin rights.<br><br><br><br>";
+if (file_exists('../config/db.php.inc')) {
+    if (isset($_SESSION['useradmin'])) {
+        if ($_SESSION['useradmin'] != 1) {
+            $logger->info('Access of upgrade page not granted! User does not have admin priviliges.', __FILE__, __LINE__);
+            echo "<br><br><br>Admin privilege needed to be able to update sessionweb. Please login using a user that have admin rights.<br><br><br><br>";
 
+        } else {
+            showUpgradeForm($versions);
+        }
     } else {
-        showUpgradeForm();
+        $logger->info('Access of upgrade page not granted! User is not logged in.', __FILE__, __LINE__);
+        echo "<br><br><br>Admin privilege needed to be able to update sessionweb. Please login using a user that have admin rights.<br><br><br><br>";
     }
 } else {
-    $logger->info('Access of upgrade page not granted! User is not logged in.', __FILE__, __LINE__);
-    echo "<br><br><br>Admin privilege needed to be able to update sessionweb. Please login using a user that have admin rights.<br><br><br><br>";
+    echo "Can not upgrade sessionweb until it is installed and a new version exist.";
 }
 
 
-function  showUpgradeForm()
+function  showUpgradeForm($versions)
 {
 
 
     if (!isset($_POST['dbadminuser']) || !isset($_POST['dbadminpassword']))
-        echoForm();
+        echoForm($versions);
     else {
         echo '                <fieldset>
                 <legend>Upgrading to latest sessionweb</legend>
                 <dl>
                     <dd>';
-        upgrade();
+        upgrade($versions);
+        echo "<div><a href='../index.php'>Back to sessionweb</a></div>";
+
         echo '    </dd>
                 </dl>
             </fieldset>';
@@ -47,27 +73,11 @@ function  showUpgradeForm()
     echo '</html>';
 }
 
-function upgrade()
+function upgrade($versions)
 {
     $logger = new logging();
     $adminuser = $_POST['dbadminuser'];
     $adminpassword = $_POST['dbadminpassword'];
-
-    $versions = array();
-    $versions['1.0'] = "SessionwebDbLayoutDelta_1.0-_1.2.sql";
-    $versions['1.2'] = "SessionwebDbLayoutDelta_1.2-_1.3.sql";
-    $versions['1.3'] = "SessionwebDbLayoutDelta_1.3-_1.4.sql";
-    $versions['1.4'] = "SessionwebDbLayoutDelta_1.4-_1.5.sql";
-    $versions['1.5'] = "SessionwebDbLayoutDelta_1.5-_1.6.sql";
-    $versions['1.6'] = "SessionwebDbLayoutDelta_1.6-_1.7.sql";
-    $versions['1.7'] = "SessionwebDbLayoutDelta_1.7-_18.sql";
-    $versions['18'] = "SessionwebDbLayoutDelta_18-_19.sql";
-    $versions['19'] = "SessionwebDbLayoutDelta_19-_20.sql";
-    $versions['20'] = "SessionwebDbLayoutDelta_20-_21.sql";
-    $versions['21'] = "SessionwebDbLayoutDelta_21-_22.sql";
-    $versions['22'] = "SessionwebDbLayoutDelta_22-_23.sql";
-    $versions['23'] = "SessionwebDbLayoutDelta_23-_24.sql";
-
 
     $messages = array();
     $messages['1.7'] = "If you got errors like 'Error Msg: Cannot delete or update a parent row: a foreign key constraint fails' during upgrade please execute /install/addFullTextSearchFromInnoDb.sql manually";
@@ -102,7 +112,7 @@ function upgrade()
                 }
                 //Do recursive upgrade until latest version...
                 if ($versionAfterUpgrade != $currentVersion && array_key_exists($versionAfterUpgrade, $versions)) {
-                    upgrade();
+                    upgrade($versions);
                 }
 
             } else {
@@ -115,7 +125,6 @@ function upgrade()
                     $logger->error("$oneError", __FILE__, __LINE__);
                 }
             }
-            echo "<div><a href='../index.php'>Back to sessionweb</a></div>";
 
 
         } elseif (tryDbConnection($adminuser, $adminpassword)) {
@@ -128,10 +137,12 @@ function upgrade()
             $logger->error("UPGRADE: Could not connect to MySql database, please check your user and password", __FILE__, __LINE__);
         }
 
+
     } else {
         echo "You already have the latest version.";
         $logger->info("UPGRADE: Upgrade aborted since the latest version is already installed", __FILE__, __LINE__);
     }
+
 
 }
 
@@ -153,7 +164,7 @@ function tryDbConnection($user, $password, $host = 'localhost')
     }
 }
 
-function echoForm()
+function echoForm($versions)
 {
     if (isset($_GET['install']) && strstr($_GET['install'], "yes") != false) {
         echo "<p>Some of the fields was empty, please fill all fields and try again.</p>";
@@ -177,17 +188,22 @@ function echoForm()
     $currentversion = getSessionWebVersion();
     if ($currentversion == null)
         $currentversion = "1.0";
-    echo "<b>Make sure that you have made a backup of your database before upgrade!</b><br>";
 
+    $last_version = key(array_slice($versions, -1, 1, TRUE)) + 1;
+
+    echo "<b>Make sure that you have made a backup of your database before upgrade!</b><br>";
+    echo 'One example of how to create a backup: mysqldump -u root -p[root_password] ' . DB_NAME_SESSIONWEB . ' > dumpfilename.sql<br><br>';
     echo "Current sessionweb version: " . $currentversion . "<br>";
-    echo "Will upgrade to latest released.";
+    echo "Will upgrade to release $last_version.";
     echo '          </dd>
                 </dl>
             </fieldset>
             <fieldset>
                 <legend>Database admin credentials</legend>
                 <dl>
-                    <dd>First of all: Make sure you have a backup of the database!! <br><br>
+                    <dd>First of all: Make sure you have a backup of the database!! <br>
+
+                    <br><br>
                     This is the user that will update the database sessionwebos and create all tables etc in the database.
                     </dd>
 
